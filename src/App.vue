7cf09c4d427beaad2ac10217adec9cd4f1ef3291
sou <sou.koga@gmail.com>
2024-06-15 14:54:55 +0900

    スライドバーを重ねて表示する機能を追加

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
-  <div class="container-fluid">
+  <div class="container-fluid position-relative">
     <div class="row flex-nowrap">
-      <Sidebar></Sidebar>
+      <Sidebar />
       <main class="col">
-        <button type="button" class="btn btn-dark mb-3 mt-3" data-bs-toggle="collapse" data-bs-target="#menuSidebar">サイドバー</button>
+        <button type="button" class="btn btn-dark open-button" data-bs-toggle="collapse" data-bs-target="#menuSidebar">
+          <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-align-justified"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 6l16 0" /><path d="M4 12l16 0" /><path d="M4 18l12 0" /></svg>
+        </button>
         <router-view></router-view>
       </main>
     </div>
   </div>
-  
 </template>
 
 <script>
@@ -22,3 +23,14 @@ export default {
 }
 </script>
 
+<style>
+
+.open-button {
+  color: white;
+  font-size: 1rem;
+  border-radius: 10px;
+  padding: 6px 12px;
+  margin-top: 10px;
+}
+
+</style>
